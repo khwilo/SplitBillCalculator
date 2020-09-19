67907e38caeb80115e49298cc2fb273a151d8fcd
Khwilo Kabaka <khwilowatai@gmail.com>
2020-09-19 11:48:19 +0300

    Manage state and handle user input

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 
 import Input from '../components/Input';
 import SplitOutput from '../containers/SplitOutput';
@@ -13,44 +18,70 @@ const styles = StyleSheet.create({
 });
 
 const Home = () => {
-  const bill = '0.00';
-  const tip = '10';
-  const split = '2';
-  const splitTotal = '0.00';
+  const [bill, setBill] = useState('0.00');
+  const [tip, setTip] = useState('10');
+  const [split, setSplit] = useState('2');
+  const [splitTotal, setSplitTotal] = useState('0.00');
+
+  useEffect(() => {
+    const defaultValue = 0;
+    const total = parseFloat(bill) || defaultValue;
+    const tipPercentage = parseInt(tip) || defaultValue;
+    const splitCount = parseInt(split) || defaultValue;
+
+    const splitTotal = (
+      (total + (tipPercentage / 100) * total) /
+      splitCount
+    ).toFixed(2);
+
+    setSplitTotal(splitTotal.toString());
+  }, [bill, tip, split]);
 
   const handleBillChange = (value) => {
-    console.log(value);
+    setBill(value);
   };
 
   const handleTipChange = (value) => {
-    console.log(value);
+    setTip(value);
   };
 
-  const handleSplitAdd = () => {};
+  const handleSplitAdd = () => {
+    setSplit((split) => {
+      const total = parseInt(split) + 1;
+      return total.toString();
+    });
+  };
 
-  const handleSplitRemove = () => {};
+  const handleSplitRemove = () => {
+    setSplit((split) => {
+      const total = parseInt(split) - 1;
+      return total.toString();
+    });
+  };
 
   return (
-    <View style={styles.container}>
-      <Input
-        label='Bill Total'
-        amount={bill}
-        placeHolderText='0.00'
-        handleTextChange={handleBillChange}
-      />
-      <Input
-        label='Tip'
-        amount={tip}
-        placeHolderText='10'
-        handleTextChange={handleTipChange}
-      />
-      <SplitOutput
-        split={split}
-        splitTotal={splitTotal}
-        handleSplitAdd={handleSplitAdd}
-        handleSplitRemove={handleSplitRemove}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Input
+          label='Bill Total'
+          amount={bill}
+          placeHolderText='0.00'
+          handleTextChange={handleBillChange}
+        />
+        <Input
+          label='Tip'
+          amount={tip}
+          placeHolderText='10'
+          handleTextChange={handleTipChange}
+        />
+        <SplitOutput
+          split={split}
+          splitTotal={splitTotal}
+          handleSplitAdd={handleSplitAdd}
+          handleSplitRemove={handleSplitRemove}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
